fac40a764031655c038b1ce201137b6ac7571dc6
Oleg Skornyakov <ideseers@gmail.com>
2022-08-25 22:20:44 +0300

    Made up ProfileView and connect with code

---
+++ ios-homeworks/ProfileView.swift
@@ -0,0 +1,19 @@
+//
+//  ProfileView.swift
+//  ios-homeworks
+//
+//  Created by Oleg Skornyakov on 8/25/22.
+//
+
+import UIKit
+
+class ProfileView: UIView {
+
+    @IBOutlet weak var avatarImageView: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var birthdayLabel: UILabel!
+    @IBOutlet weak var cityLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UITextView!
+    
+
+}
