68e7799b9e8e32de6a6f3ddd90e7ba473377469b
Oleg Skornyakov <ideseers@gmail.com>
2022-08-25 21:49:51 +0300

    Initial Commit

---
+++ ios-homeworks/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ios-homeworks
+//
+//  Created by Oleg Skornyakov on 8/25/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
